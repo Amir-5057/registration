d579b45dcc97fec1138618a067e82884267dd32b
Amir-5057 <albina.abdu.1586@gmail.com>
2024-08-27 23:48:28 +0500

    swagger

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from django.urls import path
+from registration import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', views.user_list),
+    path('user/<int:id>/', views.user_detail),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
